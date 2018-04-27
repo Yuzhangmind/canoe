c0f4866d6c32c9c9722b4d4909fb8ccc4b5f8bcf
Yuzhangmind <Yuzhangmind@users.noreply.github.com>
2018-04-27 11:22:21 +0800

    Python

---
+++ sss.py
@@ -0,0 +1,20 @@
+a=2
+b=0
+print(type(a))
+c=complex(4,5)
+i=1
+a=input()
+a=int(a)
+while True:
+    
+    while True:
+        
+        a=int(a)
+        a*=a
+        c*=c
+        print(a)
+        print(c)
+        i+=1
+        if i>15:
+           break
+                
